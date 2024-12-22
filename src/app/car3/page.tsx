a3989fa897e2d60d87610c18f220c0e7b64c208b
Sami Ullah <samiullahmalik7474@gmail.com>
2024-12-22 20:34:26 +0500

    2

--- src/app/car3/page.tsx
+++ src/app/car3/page.tsx
@@ -7,7 +7,7 @@ const page = () => {
     <div className='bg-blue-100 text-center flex flex-col gap-7 pt-10 h-full'>
         <p className='text-center font-bold underline text-2xl'>Toyota Corolla 2024 Price in Pakistan, Images, Reviews & Specs</p>
         <Image src="/35.jpg" alt="/" width={500} height={500} className='mx-auto h-52 w-auto'></Image>
-        <div className='flex w-fit mx-auto gap-1'>
+        <div className='flex w-fit md:flex-row flex-col mx-auto gap-1'>
             <button className='text-white bg-blue-600 py-2 px-6 rounded-sm text-xs'>Make Payment</button>
             <button className='text-black bg-white border-blue-600 border py-2 px-6 rounded-sm text-xs'>Make Payment</button>
             <button className='text-black bg-white border-blue-600 border py-2 px-6 rounded-sm text-xs'>Make Payment</button>
@@ -16,7 +16,7 @@ const page = () => {
             </div>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates eos dicta cupiditate dolor ipsum excepturi minus velit amet magni! Quas, consequuntur ad? Amet nam unde rem quasi explicabo culpa et?</p>
         <p className='font-semibold text-lg'>Vehicle Description</p>
-        <div className='flex w-fit mx-auto'>
+        <div className='flex w-fit md:flex-row flex-col mx-auto'>
         <p className='text-xs'><span className='font-bold'>Number of Doors: </span>4</p>
         <p className='text-xs'><span className='font-bold'> Engine:</span>1800 cc</p>
         <p className='text-xs'><span className='font-bold'> Condition: </span>8.5 / 10</p>
@@ -28,7 +28,7 @@ const page = () => {
 
         </div>
         <p className='text-green-500 text-lg'>PKR 50&apos;000</p>
-        <button className='text-white bg-blue-600 py-3 px-7 rounded-md mx-auto'><Link href="/form">Make Payment</Link></button>
+        <button className='text-white bg-blue-600 mb-2 py-3 px-7 rounded-md mx-auto'><Link href="/form">Make Payment</Link></button>
     </div>
   )
 }
--- src/app/thanks/page.tsx
+++ src/app/thanks/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <div className='flex gap-7 pt-60 flex-col bg-blue-100 h-screen'>
-        <h1 className='w-fit mx-auto text-4xl text-green-400 font-bold'>Thank you for shopping</h1>
+        <h1 className='w-fit mx-auto md:text-4xl text-2xl text-green-400 font-bold'>Thank you for shopping</h1>
         <p className='w-fit mx-auto'>your order will be delevered within three days</p>
     </div>
   )
